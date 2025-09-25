4d1ef6fc698558618a21bbf4a6b6b9c026ea6bc2
Micheal Ngene <mngene4@gmail.com>
2025-09-25 05:32:53 +0200

    Added configuration for deployment

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -3,7 +3,7 @@ import { Redis } from "@upstash/redis";
 
 const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(5, "60 s"),
+    limiter: Ratelimit.slidingWindow(100, "60 s"),
 });
 
 export default ratelimit;
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,16 +3,29 @@ import cors from "cors";
 import noteRoutes from "./routes/noteRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middlewares/rateLimiter.js";
+import path from "path";
 
 const app = express();
 const port = process.env.PORT || 3005;
+const __dirname = path.resolve();
 
 app.use(express.json());
-app.use(cors({
-    origin: "http://localhost:5173",
-}));
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173",
+    }));
+}
 app.use(rateLimiter);
 app.use("/api/notes", noteRoutes);
+
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.use((_, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 connectDB().then(() => {
     app.listen(port, () => {
